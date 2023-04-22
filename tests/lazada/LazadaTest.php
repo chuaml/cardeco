7dfc02ddfcb7b161b7710519a4b83b4ec4217fd5
chuaml <chuaml@outlook.com>
2023-04-23 02:04:39 +0800

    refactor tests

--- tests/lazada/LazadaTest.php
+++ tests/lazada/LazadaTest.php
@@ -38,30 +38,4 @@ final class LazadaTest extends TestCase
         $orders = json_encode($orders, JSON_PRETTY_PRINT);
         $this->assertEquals($orders, $expectedResult);
     }
-
-    public function testConvertToSqlImport_MonthlyCashSalesRecord_ValidConstantValue()
-    {
-        $con = require 'tests/db.connection.php';
-        $testInputFilePath = 'tests/lazada/data.input/lazada.monthly.cashsales.record.xlsx';
-        $xlsx = new ExcelReader($testInputFilePath);
-        $paymentType = 'Lazada';
-        $startRowPos = 1;
-        $lastRowPos = -1;
-        $rows = $xlsx->read($paymentType, $startRowPos, $lastRowPos);
-
-        $list = CashSales::transformToCashSales($con, $paymentType, iterator_to_array($rows));
-
-        foreach ($list as $i => $x) {
-            try {
-                $this->assertEquals('300-C0006', $x['Code(10)']);
-                $this->assertEquals('CASH A/C - LAZADA (CAR DECO)', $x['CompanyName(100)']);
-                $this->assertEquals('321-000', $x['P_PAYMENTMETHOD']);
-            } catch(\Throwable $ex) {
-                echo 'fail at index $i: ' . $i;
-                var_dump($x);
-                throw $ex;
-            }
-        }
-        $this->assertNotEquals(0, count($list));
-    }
 }
---
+++ tests/lazada/Lazada_MonthlyCashSales_ConvertToSqlImportTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace test\lazada;
+
+use OrderProcess\LazadaOrderProcess;
+use Orders\Factory\Excel\CashSales;
+use Orders\Factory\Excel\ExcelReader;
+use Orders\PaymentCharges\BankIn;
+use Orders\PaymentCharges\Cash;
+use Orders\PaymentCharges\Lazada;
+use PhpParser\Node\Stmt\TryCatch;
+use PHPUnit\Framework\TestCase;
+
+final class Lazada_MonthlyCashSales_ConvertToSqlImportTest extends TestCase
+{
+    private function getMonthlyCashSalesRecord_As_SqlImport($testInputFilePath, $platformName): array
+    {
+        $con = require 'tests/db.connection.php';
+        $xlsx = new ExcelReader($testInputFilePath);
+        $startRowPos = 1;
+        $lastRowPos = -1;
+        $rows = $xlsx->read($platformName, $startRowPos, $lastRowPos);
+
+        return CashSales::transformToCashSales($con, $platformName, iterator_to_array($rows));
+    }
+
+    public function testPlatformCharges_Lazada_ValidConstantValue()
+    {
+        $chargeType = new Lazada(0.00);
+        $platformName = $chargeType->getPlatform();
+        $testInputFilePath = 'tests/lazada/data.input/lazada.monthly.cashsales.record.xlsx';
+
+        $list = $this->getMonthlyCashSalesRecord_As_SqlImport($testInputFilePath, $platformName);
+
+        $this->assertNotEquals(0, count($list));
+        foreach ($list as $i => $x) {
+            try {
+                $this->assertEquals($chargeType->getCustId(), $x['Code(10)']);
+                $this->assertEquals($chargeType->getCompanyName(), $x['CompanyName(100)']);
+                $this->assertEquals($chargeType->getPaymentInto(), $x['P_PAYMENTMETHOD']);
+            } catch (\Throwable $ex) {
+                echo 'fail at index $i: ' . $i;
+                var_dump($x);
+                throw $ex;
+            }
+        }
+    }
+
+}
