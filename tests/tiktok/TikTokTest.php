c699b2c67bbeb745ba966dcc05eb694a2252acc9
chuaml <chuaml@outlook.com>
2023-04-23 02:15:07 +0800

    refactor tiktok test

--- tests/tiktok/TikTokTest.php
+++ tests/tiktok/TikTokTest.php
@@ -6,6 +6,7 @@ use IO\CSVInputStream;
 use OrderProcess\TikTokOrder;
 use Orders\Factory\Excel\CashSales;
 use Orders\Factory\Excel\ExcelReader;
+use Orders\PaymentCharges\TikTok_Eplus;
 use PHPUnit\Framework\TestCase;
 
 final class TikTokTest extends TestCase
@@ -49,22 +50,30 @@ final class TikTokTest extends TestCase
 
         $orders = $q->getOrders();
         $orders = json_encode($orders, JSON_PRETTY_PRINT);
+
         $this->assertEquals($expectedResult, $orders);
+    }
+
+    private function getMonthlyCashSalesRecord_As_SqlImport($testInputFilePath, $platformName): array
+    {
+        $con = require 'tests/db.connection.php';
+        $xlsx = new ExcelReader($testInputFilePath);
+        $startRowPos = 1;
+        $lastRowPos = -1;
+        $rows = $xlsx->read($platformName, $startRowPos, $lastRowPos);
+
+        return CashSales::transformToCashSales($con, $platformName, iterator_to_array($rows));
     }
 
     public function testConvertToSqlImport_MonthlyCashSalesRecord_ValidConstantValue()
     {
-        $con = require 'tests/db.connection.php';
+        $chargeType = new TikTok_Eplus(0.00);
+        $platformName = $chargeType->getPlatform();
         $testInputFilePath = 'tests/tiktok/data.input/tiktok.monthly.cashsales.record.sample.xlsx';
-        $xlsx = new ExcelReader($testInputFilePath);
-        $platformType = 'TikTok_Eplus';
-        $startRowPos = 1;
-        $lastRowPos = -1;
-        $rows = $xlsx->read($platformType, $startRowPos, $lastRowPos);
 
-        $list = CashSales::transformToCashSales($con, $platformType, iterator_to_array($rows));
+        $list = $this->getMonthlyCashSalesRecord_As_SqlImport($testInputFilePath, $platformName);
 
-        $this->assertTrue(count($list) > 0);
+        $this->assertGreaterThan(0, count($list));
         foreach ($list as $x) {
             $this->assertTrue($x['Code(10)'] === '300-C0013');
             $this->assertTrue($x['CompanyName(100)'] === 'CASH A/C - TIKTOK (E PLUS)');
