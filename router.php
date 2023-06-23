e94148bee536335fdc9635a53c1b809522ecb9f1
chuaml <chuaml@outlook.com>
2023-06-23 23:45:09 +0800

    remove dead code

--- router.php
+++ router.php
@@ -1,35 +1,18 @@
 <?php
-
 require 'vendor/autoload.php';
-
-
-// define routes
-// $routes = require 'routes.php';
-
-// get requested URI
-// $requestUri = $_SERVER['REQUEST_URI'];
-
-// // check if the requested URI exists in the routes array
-// if (array_key_exists($requestUri, $routes) === true) {
-//     // if it does, require the corresponding PHP file
-
-//     require $routes[$requestUri];
-// } else {
-//     // if it doesn't, show a 404 error page
-//     header('HTTP/1.0 404 Not Found');
-//     echo '404 Not Found';
-// }
 
 use Exception\HttpException;
 
 try {
     require(__DIR__ . '/db/conn_staff.php');
 
+    // default route
     if ($_SERVER['REQUEST_URI'] === '/') {
         require 'request_handler/lazada.php';
         return;
     }
 
+    // other route
     $_requestUri = 'request_handler' . $_SERVER['REQUEST_URI'];
     if (is_dir($_requestUri) === true) {
         $_requestUri .= 'index.php';
@@ -48,7 +31,6 @@ try {
     if ($statusCode === 404) {
         header("HTTP/1.1 404 Not Found");
         var_dump($ex);
-        
     } else if ($statusCode === 500) {
         header("HTTP/1.1 500 Internal Server Error");
     } else {
