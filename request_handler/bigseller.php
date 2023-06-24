d4b6c9fa9202a4dfdca3d79602526ba73a8b8ff6
chuaml <chuaml@outlook.com>
2023-06-24 19:50:45 +0800

    add ga4 ecom tracking for bigseller

--- request_handler/bigseller.php
+++ request_handler/bigseller.php
@@ -12,11 +12,18 @@ $Data = [
     'orders' => ''
 ];
 $msg = '';
+
+$jsonOrders = '';
+$dailyOrderFile_Sha1Hash = '';
+
 try {
     if (isset($_FILES['dailyOrders'])) {
         try {
             $L = new BigSellerOrderProcess($con, $_FILES['dailyOrders']['tmp_name']);
             $Data = $L->getData();
+
+            $jsonOrders = json_encode($L->getOrders());
+            $dailyOrderFile_Sha1Hash = sha1_file($_FILES['dailyOrders']['tmp_name']);
         } catch (Exception $e) {
             $msg = $e->getMessage();
         }
