a27de520ba4d27ebd9534adeeea251509a80de7a
chuaml <chuaml@outlook.com>
2024-04-20 14:56:47 +0800

    add a preview for CashSaleSqlImport

--- request_handler/CashSaleSqlImport.php
+++ request_handler/CashSaleSqlImport.php
@@ -1,33 +1,36 @@
 <?php
+
 namespace main;
 
 use Exception;
+use HTML\TableDisplayer;
 use InvalidArgumentException;
 use Orders\Factory\Excel\CashSales;
 use Orders\Factory\Excel\SqlImport;
 use Orders\Factory\Excel\ExcelReader;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-function validateFile(array $file):void
+function validateFile(array $file): void
 {
     if (count($file) !== 5) {
         throw new InvalidArgumentException('expected $_FILES[0]');
     }
-    
+
     if ($file['error'] !== 0) {
         throw new Exception('file upload error.');
     }
 
     $uploadedFileExt = pathinfo(basename($file['name']), PATHINFO_EXTENSION);
     if ($uploadedFileExt !== 'xlsx') {
-        throw new Exception('invalid file extension: ' .$uploadedFileExt);
+        throw new Exception('invalid file extension: ' . $uploadedFileExt);
     }
 }
 
+$table = null;
 $errmsg = '';
 try {
     if (isset($_POST['fileTab']) && isset($_FILES['dataFile'])) {
-        $file =& $_FILES['dataFile'];
+        $file = &$_FILES['dataFile'];
         $fileTab = $_POST['fileTab'] ?? null;
         $startRowPos = $_POST['startRowPos'] ?? 1;
         $lastRowPos = $_POST['lastRowPos'] ?? -1;
@@ -37,24 +40,32 @@ try {
 
         $rows = (new ExcelReader($file['tmp_name']))->read($fileTab, $startRowPos, $lastRowPos);
         $list = CashSales::transformToCashSales($con, $fileTab, iterator_to_array($rows));
+
+        $table = new TableDisplayer($list, 'tblCashSaleImport');
+    } else if (isset($_POST['jsonDataToExport'])) {
+        $json = $_POST['jsonDataToExport'];
+        $list = json_decode($json);
+        $list = array_map(function ($x) {
+            return (array) $x;
+        }, $list);
         $Spreadsheet = SqlImport::loadSpreadsheet($list);
         if (error_get_last() !== null) {
             http_response_code(500);
             throw new Exception('Some Errors have occoured.');
         }
 
-        // Redirect output to a client’s web browser (Xlsx)
+        // // Redirect output to a client’s web browser (Xlsx)
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="CashSales for SQL Import.xlsx"');
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
-    
+
         // If you're serving to IE over SSL, then the following may be needed
         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
         header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
         header('Pragma: public'); // HTTP/1.0
-    
+
         $writer = IOFactory::createWriter($Spreadsheet, 'Xlsx');
         $writer->save('php://output');
         exit();
