4cff5c22c151f44f5415981276db4a9fa73e0c89
chuaml <chuaml@outlook.com>
2025-01-11 20:48:26 +0800

    fix abandon unchanges confirmation

--- js/RecordEditor.js
+++ js/RecordEditor.js
@@ -17,15 +17,31 @@ const targetColId = [
     "cash"
 ];
 
+{ // unsave changes confirmation
+    let hasChanges = false;
+    window.addEventListener('beforeunload', function (e) {
+        const currentInput = document.activeElement;
+        try {
+            currentInput.blur(); // focus emit last 'change' event
+        } catch (err) { console.error(err); }
 
-$("form#RecordEditorForm").submit(function () {
-    if (confirm("confirm save changes?")) {
-        $(window).unbind("beforeunload");
-    } else {
-        return false;
-    }
-});
+        if (hasChanges === false) return;
+        e.preventDefault();
+        this.dispatchEvent(new Event('pageshow')); // cancel loading screen
+        this.setTimeout(_ => currentInput.focus(), 0); // focus back last 'change' input
 
+        e.returnValue = 'abandon unsaved changes?';
+        return 'abandon unsaved changes?';
+    }, true);
+
+    document.addEventListener('change', function (e) {
+        hasChanges = true;
+    }, { passive: true });
+
+    document.addEventListener('submitted', function (e) {
+        hasChanges = false;
+    }, { passive: true });
+}
 
 //hideShowCol function
 //bind to btn
