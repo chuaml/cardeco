19d6a20b8bb2dab805d9003bb773b5b725596c88
chuaml <chuaml@outlook.com>
2025-05-18 02:13:42 +0800

    add analytics

--- view/template/head.php
+++ view/template/head.php
@@ -41,6 +41,19 @@
 		gtag('config', 'G-139719LGJQ');
 	</script>
 
+	<script type="text/javascript">
+		(function(c, l, a, r, i, t, y) {
+			c[a] = c[a] || function() {
+				(c[a].q = c[a].q || []).push(arguments)
+			};
+			t = l.createElement(r);
+			t.async = 1;
+			t.src = "https://www.clarity.ms/tag/" + i;
+			y = l.getElementsByTagName(r)[0];
+			y.parentNode.insertBefore(t, y);
+		})(window, document, "clarity", "script", "ri0as4ewcy");
+	</script>
+
 <?php } ?>
 
 <script>
